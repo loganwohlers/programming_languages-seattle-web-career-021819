440aa40e85ad55397596c7cd7339729af5196e79
Logan Wohlers <lawohlers@gmail.com>
2019-02-07 22:16:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  nhash={}
+  languages.each do |k, v|
+    v.each do |k2, v2|
+      if(!nhash.keys.include?(k2))
+        nhash[k2]=v2
+        nhash[k2][:style]=[k]
+      else
+        nhash[k2][:style].push(k)
+      end
+    end
+  end
+  nhash
 end
